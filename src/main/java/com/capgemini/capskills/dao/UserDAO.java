90401f8c12d3f0a5907e68f4e7f00a3f58f457c1
Quentin Prigent <qp.prigent@gmail.com>
2017-12-18 14:38:17 +0100

    Bugs are fixed

--- src/main/java/com/capgemini/capskills/dao/UserDAO.java
+++ src/main/java/com/capgemini/capskills/dao/UserDAO.java
@@ -40,16 +40,4 @@ public class UserDAO extends BaseDAO<User> implements IUserDAO {
 	public void update(User item) {
 		entityManager.merge(item);
 	}
-	
-	public User getByEmail(String email) {
-		return (User) this.entityManager.createQuery("SELECT u FROM User u WHERE u.email=:email").setParameter("email", email).setMaxResults(1).getSingleResult();
-	}
-
-
-	@Override
-	public Query createQuery(String qlString) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
